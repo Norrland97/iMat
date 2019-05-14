6f008bf071ff771bb99607422548bc693742da93
klauzr <pontus.toender@gmail.com>
2019-05-14 10:53:07 +0200

    Kommentar
    Jag skrev hej

--- src/IMat.java
+++ src/IMat.java
@@ -2,8 +2,7 @@ import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 public class IMat {
     public static void main(String[] args){
-        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-
+        IMatDataHandler dataHandler = IMatDataHandler.getInstance(); // hej alla
     }
 
 }
