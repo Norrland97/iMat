d55183a04f4dd27e002072e934fbecb7063482f7
Hentoo <45872428+Hentoo@users.noreply.github.com>
2019-05-14 10:34:28 +0200

    Update IMat.java

--- src/IMat.java
+++ src/IMat.java
@@ -3,7 +3,7 @@ import se.chalmers.cse.dat216.project.IMatDataHandler;
 public class IMat {
     public static void main(String[] args){
         IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-        System.out.println("sup");
+
     }
 
 }
