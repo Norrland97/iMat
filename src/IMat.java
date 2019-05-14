e63c224c2d1ef8d7c86c3cbd592742597532ef4b
Hentoo <45872428+Hentoo@users.noreply.github.com>
2019-05-14 10:15:03 +0200

    Nu kör vi boys

---
+++ src/IMat.java
@@ -0,0 +1,8 @@
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+
+public class IMat {
+    public static void main(String[] args){
+        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
+    }
+
+}
