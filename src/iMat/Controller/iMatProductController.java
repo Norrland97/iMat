b74fddb36fd4386a62fdd0012d921a3bb169feb9
Viktor <fredholm97@gmail.com>
2019-05-15 17:03:17 +0200

    jobbat på

---
+++ src/iMat/Controller/iMatProductController.java
@@ -0,0 +1,26 @@
+package iMat.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import javax.swing.text.html.ImageView;
+
+public class iMatProductController extends AnchorPane {
+
+    @FXML
+    private ImageView imageField;
+
+    @FXML
+    private Button lesserButon;
+
+    @FXML
+    private Button moreButton;
+
+    @FXML
+    private Button addToCartButton;
+
+    @FXML
+    private TextField nrOfItems;
+}
