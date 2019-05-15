f9c64b3b33c81275020352a7f0197ec897d1672d
Viktor <fredholm97@gmail.com>
2019-05-15 11:20:20 +0200

    jobb på backendController

--- src/Controller/iMatBackendController.java
+++ src/Controller/iMatBackendController.java
@@ -1,7 +1,22 @@
 package Controller;
 
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
+import se.chalmers.cse.dat216.project.ProductCategory;
+
+import java.util.List;
+
 public class iMatBackendController {
 
+    ProductCategory category;
+
+
+
+    IMatDataHandler iMatData = IMatDataHandler.getInstance();
+
+    public List<Product> getProducts (){
+
+    }
 
 
 }
