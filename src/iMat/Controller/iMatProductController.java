314a6bbbf843f8278b8a587c8ea21df0604cf6ed
Viktor <fredholm97@gmail.com>
2019-05-16 14:52:57 +0200

    work on product

--- src/iMat/Controller/iMatProductController.java
+++ src/iMat/Controller/iMatProductController.java
@@ -5,17 +5,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
 
-import javax.swing.text.html.ImageView;
-import javax.xml.soap.Text;
 import java.io.IOException;
 
 public class iMatProductController extends AnchorPane {
     @FXML
     private ImageView imageField;
+
+    @FXML
+    private ImageView favoriteStar;
 
     @FXML
     private Button lesserButon;
