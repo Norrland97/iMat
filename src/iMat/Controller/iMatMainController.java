9a315d3f0b0dd0d7612c8823b00b85510b0b9684
Viktor <fredholm97@gmail.com>
2019-05-17 11:22:32 +0200

    worked on connecting the backend to the project

--- src/iMat/Controller/iMatMainController.java
+++ src/iMat/Controller/iMatMainController.java
@@ -1,10 +1,14 @@
 package iMat.Controller;
 
+
+import iMat.Model.iMatModel;
 import javafx.fxml.FXML;
 import javafx.scene.layout.FlowPane;
+import se.chalmers.cse.dat216.project.CreditCard;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.Product;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +19,7 @@ public class iMatMainController {
     @FXML
     FlowPane productsFlowPane;
 
+    private final iMatModel model = iMatModel.getInstance();
 
 
     private void updateRecipeList(){
@@ -22,5 +27,25 @@ public class iMatMainController {
         for(Product product : IMatDataHandler.getInstance().getProducts()){
             productsFlowPane.getChildren().add(recipeListItemMap.get(product.getName()));
         }
+    }
+
+    private void updateCreditCard() {
+
+        CreditCard card = model.getCreditCard();
+
+        card.setCardNumber(numberTextField.getText());
+        card.setHoldersName(nameTextField.getText());
+
+        String selectedValue = (String) cardTypeCombo.getSelectionModel().getSelectedItem();
+        card.setCardType(selectedValue);
+
+        selectedValue = (String) monthCombo.getSelectionModel().getSelectedItem();
+        card.setValidMonth(Integer.parseInt(selectedValue));
+
+        selectedValue = (String) yearCombo.getSelectionModel().getSelectedItem();
+        card.setValidYear(Integer.parseInt(selectedValue));
+
+        card.setVerificationCode(Integer.parseInt(cvcField.getText()));
+
     }
 }
---
+++ src/iMat/Model/iMatModel.java
@@ -0,0 +1,38 @@
+package iMat.Model;
+
+import se.chalmers.cse.dat216.project.CreditCard;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class iMatModel {
+
+    private static iMatModel instance = null;
+    private IMatDataHandler iMatDataHandler;
+
+    /**
+     * info som fylls i för bankkort
+     */
+    private final ArrayList<String> availableCardTypes = new ArrayList<String>(Arrays.asList("MasterCard", "Visa"));
+    private final ArrayList<String> months = new ArrayList<String>(Arrays.asList("1", "2","3", "4", "5", "6"));
+    private final ArrayList<String> years = new ArrayList<String>(Arrays.asList("19", "20", "21", "22", "23", "24", "25"));
+
+    protected iMatModel(){}
+
+    public static iMatModel getInstance() {
+        if (instance == null){
+            instance = new iMatModel();
+            instance.init();
+        }
+        return instance;
+    }
+
+    private void init() {
+        iMatDataHandler = IMatDataHandler.getInstance();
+    }
+
+    public CreditCard getCreditCard() {
+        return iMatDataHandler.getCreditCard();
+    }
+}
